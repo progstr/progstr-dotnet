45d80dc98666a32950ac242a2bc9fedb15f8d37a
Hristo Deshev <hristo@deshev.com>
2011-03-14 22:09:01 +0200

    Added the MVC log example.

---
+++ src/web-example/Controllers/LogExampleController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Progstr.Log;
+using System.Diagnostics;
+
+namespace WebExample.Controllers
+{
+    [HandleError]
+    public class LogExampleController : Controller
+    {
+        public ActionResult Index()
+        {
+            this.Log().Info("Welcome to the ASP.NET MVC Progstr.Log example.");
+
+            ViewData["Message"] = "Welcome to the ASP.NET MVC Progstr.Log example.";
+            return View();
+        }
+        
+        public ActionResult Log(string logMessage, string severity)
+        {
+            switch (severity)
+            {
+                case "Info": 
+                    this.Log().Info(logMessage);
+                    break;
+                case "Warning": 
+                    this.Log().Warning(logMessage);
+                    break;
+                case "Error": 
+                    this.Log().Error(logMessage);
+                    break;
+                case "Fatal": 
+                    this.Log().Fatal(logMessage);
+                    break;
+                default:
+                    this.Log().Info(logMessage);
+                    break;
+            }
+            
+            ViewData[severity] = true;
+            return View("Index");
+        }
+    }
+}
---
+++ src/web-example/Views/LogExample/Index.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Home Page
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%= ViewData["Message"] %></h2>
+    <% using (Html.BeginForm("Log", "LogExample")) { %>
+    <p>Log message:</p>
+    <p><%= Html.TextArea("logMessage", new { style = "height:150px;width:300px;" }) %></p>
+    <p>Severity:</p>
+    <p>
+        <label><%= Html.RadioButton("severity", "Info", ViewData["Info"] ?? false) %>Info</label>
+        <label><%= Html.RadioButton("severity", "Warning", ViewData["Warning"] ?? false) %>Warning</label>
+        <label><%= Html.RadioButton("severity", "Error", ViewData["Error"] ?? false) %>Error</label>
+        <label><%= Html.RadioButton("severity", "Fatal", ViewData["Fatal"] ?? false) %>Fatal</label>
+    </p>
+    <p><input type="submit" value="Log"></input></p>
+    <% } %>
+</asp:Content>
