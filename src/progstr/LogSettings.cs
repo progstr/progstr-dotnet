b79f5e3eea326151bb639965975d7d89f1ef46f2
Hristo Deshev <hristo@deshev.com>
2011-07-02 14:36:41 +0300

    Documented the LogSettings properties.

--- src/progstr/LogSettings.cs
+++ src/progstr/LogSettings.cs
@@ -3,17 +3,28 @@ namespace Progstr.Log
 {
     public static class LogSettings
     {
-        public static bool? EnableCompression
-        {
-            get; set;
-        }
-        
+        /// <summary>
+        /// Gets or sets your API access token. This parameter is mandatory - you won't be able to log otherwise. 
+        /// </summary>
         public static string ApiToken
         {
             get; set;
         }
         
+        /// <summary>
+        /// The base URL of the progstr.log service endpoint. Setting to a value different than the default may break log requests.
+        /// Don't set unless you know what you are doing or a support person has advised you to do so.
+        /// </summary>
         public static string BaseUrl
+        {
+            get; set;
+        }
+        
+        /// <summary>
+        /// Gets or sets a value indicating whether log messages will be compressed before sent to the progstr.log servers. 
+        /// The default is 'true'.
+        /// </summary>
+        public static bool? EnableCompression
         {
             get; set;
         }
