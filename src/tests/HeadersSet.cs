86ac5ba05ad3d34f373aa1c2f7a5498034fefeb6
Hristo Deshev <hristo@deshev.com>
2011-07-03 22:52:04 +0300

    Normalizing CRLF to LF in test data to make this run on both Windows and Unix.

--- src/tests/HeadersSet.cs
+++ src/tests/HeadersSet.cs
@@ -22,12 +22,13 @@ namespace Progstr.Tests
             var client = new TestClient(this.settings);
             client.ConfigureRequest(null);
             
-            client.Headers.ShouldBe(@"Accept: application/json
+            var headers = this.NormalizeLineEndings(client.Headers);
+            headers.ShouldBe(this.NormalizeLineEndings(@"Accept: application/json
 Content-Type: application/json; charset=utf-8
 Content-Encoding: gzip
 User-Agent: progstr-dotnet 1.0
 X-Progstr-Token: 7cf122aa-6df5-4fba-a5b3-f6f0a54e7b01
-");
+"));
         }
         
         [Fact]
@@ -37,11 +38,17 @@ X-Progstr-Token: 7cf122aa-6df5-4fba-a5b3-f6f0a54e7b01
             var client = new TestClient(this.settings);
             client.ConfigureRequest(null);
             
-            client.Headers.ShouldBe(@"Accept: application/json
+            var headers = this.NormalizeLineEndings(client.Headers);
+            headers.ShouldBe(this.NormalizeLineEndings(@"Accept: application/json
 Content-Type: application/json; charset=utf-8
 User-Agent: progstr-dotnet 1.0
 X-Progstr-Token: 7cf122aa-6df5-4fba-a5b3-f6f0a54e7b01
-");
+"));
+        }
+
+        public string NormalizeLineEndings(string input)
+        {
+            return input.Replace("\r", "");
         }
     }
 }
